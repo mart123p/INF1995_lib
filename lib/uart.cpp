836fc6a7204216d0f3c96dd4991a1b0d6f4f950a
Martin Pouliot <martin.pouliot@polymtl.ca>
2018-02-16 10:27:21 -0500

    Change the array to const

--- lib/uart.cpp
+++ lib/uart.cpp
@@ -1,6 +1,6 @@
 #include "uart.h"
 #include <avr/pgmspace.h>
-char asciiNumbers[] PROGMEM = {'0','1','2','3','4','5','6','7','8','9'};
+const char asciiNumbers[] PROGMEM = {'0','1','2','3','4','5','6','7','8','9'};
 
 
 void uartSend(uint8_t data){
