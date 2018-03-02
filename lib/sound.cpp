7eb09f755763d3dbeea7b0fc51263326728682e5
Martin Pouliot <martin.pouliot@polymtl.ca>
2018-03-02 10:00:51 -0500

    Test frequency

---
+++ lib/sound.cpp
@@ -0,0 +1,26 @@
+#include "sound.h"
+enum Note {C,C_,D,D_,E,F,F_,G,G_,A,A_,B};
+void sound::init(){
+	DDRC =| 0x0C;
+}
+/**
+ * @brief      Plays a frequency
+ *
+ * @param[in]  freq      The frequency in hertz From (1000hz to 9999hz)
+ * @param[in]  duration  The duration in ms
+ */
+void frequency(const uint16_t freq, const uint16_t duration){
+	uint16_t i = 0;
+	uint16_t time = ((freq+5) / 1000) * duration;
+	while(i != time){
+		PORTC |= (1 << PD2);
+		_delay_us(5);
+		PORTC  &= ~(1 << PD3);
+		_delay_us(freq);
+		i++;
+	}
+}
+
+void sound::test(){
+	frequency(1046,500);
+}
