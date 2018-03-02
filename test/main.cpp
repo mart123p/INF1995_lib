851375e5df6b7c10bff906f9f0a45eddef1494c7
Martin Pouliot <martin.pouliot@polymtl.ca>
2018-03-02 10:03:26 -0500

    ready for compilation

--- test/main.cpp
+++ test/main.cpp
@@ -8,32 +8,9 @@
 #include "ohBoy.h"
 int main()
 {
-		uart::test();
 		
 		uart::print("\nTesting light\n...");
-		light::test();
-		uart::print("\nDone testing light\n");
-		
-		//Memory test
-		uart::print("\nTesting Memory\n...\n");
-		mem::test();
-		uart::print("\n\nDone testing Memory\n");
-			
-		//Testing timer with LED
-		
-		//Testing can
-		
-		//Waiting for interrupt test
-		
-		//Testing sound
-		
-		uart::print("\nTesting PWM\n...");
-		pwm::test();
-		uart::print("\nDone testing PWM\n");
-	
-				
-	
-		
+		sound::test();
 		uart::print("\n\nTests were completed!");	
 		while(true);
 }
